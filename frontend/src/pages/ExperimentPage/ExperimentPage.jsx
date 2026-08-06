53921d39a1ba86232af5454ea821877d634333ce
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-06 16:33:36 +0900

    [Feature] #6 Imported MyTwoCents to Experiment Page

---
+++ frontend/src/pages/ExperimentPage/ExperimentPage.jsx
@@ -0,0 +1,9 @@
+import MyTwoCents from "../../components/conditions/MyTwoCents/components/Navigator/Navigator";
+
+export default function ExperimentPage() {
+  return (
+    <div>
+      <MyTwoCents />
+    </div>
+  );
+}
