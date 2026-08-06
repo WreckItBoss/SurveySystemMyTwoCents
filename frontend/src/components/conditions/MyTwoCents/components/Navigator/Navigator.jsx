696798f99b9c5a96bdb1870ae4556a8645a45076
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-06 16:09:02 +0900

    [Feature] #1 Added navigator content

---
+++ frontend/src/components/conditions/MyTwoCents/components/Navigator/Navigator.jsx
@@ -0,0 +1,14 @@
+// components/Navigator.jsx
+import "./Navigator.css";
+
+export default function Navigator() {
+  return (
+    <nav className="nav-bleed">
+      <div className="nav-inner">
+        <div className="nav-brand" style={{ cursor: "default" }}>
+          MyTwoCents
+        </div>
+      </div>
+    </nav>
+  );
+}
