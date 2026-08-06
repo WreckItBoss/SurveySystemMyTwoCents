c48b1ec971409238c64f313e3c6ea98165ea9f3f
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-06 17:18:22 +0900

    [Feature] #9 Added logic for ExperimentPage

--- frontend/src/pages/ExperimentPage/ExperimentPage.jsx
+++ frontend/src/pages/ExperimentPage/ExperimentPage.jsx
@@ -1,9 +1,34 @@
 import MyTwoCents from "../../components/conditions/MyTwoCents/components/Navigator/Navigator";
+import NewsOnly from "../../components/conditions/MyTwoCents/components/NewsOnly/NewsOnly";
 
-export default function ExperimentPage() {
-  return (
-    <div>
-      <MyTwoCents />
-    </div>
-  );
+export default function ExperimentPage({ assignment, onComplete}) {
+    if (!assignment) {
+        return <p>実験内容を読み込んでいます...</p>;
+    }
+
+    if (assignment.condition === "MyTwoCents"){
+        return(
+            <div>
+                <MyTwoCents 
+                    topic = {assignment.topic}
+                    pattern = {assignment.pattern}
+                    onComplete = {onComplete}
+
+                />
+            </div>
+        );
+    }
+
+    if(assignment.condition === "NewsOnly"){
+        return(
+            <div>
+                <NewsOnly 
+                    topic = {assignment.topic}
+                    onComplete = {onComplete}
+                />
+            </div>
+        );
+    }
+
+    return <p>実験条件を読み込めませんでした。</p>;
 }
