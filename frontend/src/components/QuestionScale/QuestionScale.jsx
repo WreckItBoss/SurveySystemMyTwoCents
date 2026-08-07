80bd1824850b93c4761076495d773b82c293674e
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-07 13:57:22 +0900

    [Feature] #36 made is so that it can take in objects and strings

--- frontend/src/components/QuestionScale/QuestionScale.jsx
+++ frontend/src/components/QuestionScale/QuestionScale.jsx
@@ -1,5 +1,3 @@
-import "./QuestionScale.css";
-
 export default function QuestionScale({
   label,
   options,
@@ -11,35 +9,48 @@ export default function QuestionScale({
     <fieldset className="question-scale">
       <legend className="question-scale__question">
         {label}
-
         {required && (
-          <span className="question-scale__required" aria-label="必須">
+          <span className="question-scale__required">
             *
           </span>
         )}
       </legend>
 
       <div className="question-scale__options">
-        {options.map((option) => (
-          <label
-            key={option}
-            className={`question-scale__option ${
-              value === option
-                ? "question-scale__option--selected"
-                : ""
-            }`}
-          >
-            <input
-              type="radio"
-              value={option}
-              checked={value === option}
-              onChange={() => onChange(option)}
-              required={required}
-            />
+        {options.map((option) => {
+          const optionValue =
+            typeof option === "object"
+              ? option.value
+              : option;
 
-            <span>{option}</span>
-          </label>
-        ))}
+          const optionLabel =
+            typeof option === "object"
+              ? option.label
+              : option;
+
+          return (
+            <label
+              key={optionValue}
+              className={`question-scale__option ${
+                value === optionValue
+                  ? "question-scale__option--selected"
+                  : ""
+              }`}
+            >
+              <input
+                type="radio"
+                value={optionValue}
+                checked={value === optionValue}
+                onChange={() =>
+                  onChange(optionValue)
+                }
+                required={required}
+              />
+
+              <span>{optionLabel}</span>
+            </label>
+          );
+        })}
       </div>
     </fieldset>
   );
