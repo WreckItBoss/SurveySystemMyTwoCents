cdab72c9415292714a5178df006890f66f07aad1
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-08 23:19:06 +0900

    imported session routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import responseRoutes from "./routes/responseRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
 
