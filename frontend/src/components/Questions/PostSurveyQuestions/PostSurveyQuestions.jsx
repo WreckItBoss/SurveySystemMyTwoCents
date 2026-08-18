cf74667e1637085c5c7fc564c4b00551f0d31c32
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-18 18:42:55 +0900

    [Feature] #53 Changed the wording to chatbot

--- frontend/src/components/Questions/PostSurveyQuestions/PostSurveyQuestions.jsx
+++ frontend/src/components/Questions/PostSurveyQuestions/PostSurveyQuestions.jsx
@@ -83,7 +83,7 @@ export default function createPostSurveyQuestions(topic, condition) {
 export const systemCommentQuestion = {
   key: "systemComment",
   label:
-    "今回利用した、ニュース記事とAIエージェントによる議論を表示するシステムについて、使いやすさ、読みやすさ、議論の内容、良かった点・改善してほしい点など、感じたことがあれば自由にご記入ください。",
+    "今回利用した、ニュース記事とチャットボットを表示するシステムについて、使いやすさ、読みやすさ、議論の内容、良かった点・改善してほしい点など、感じたことがあれば自由にご記入ください。",
   placeholder: "こちらにご記入ください。",
 };
 
