f0f2e887cb9037d0dee1ef8054a1853b6b7d2860
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-08-25 19:15:16 +0900

    [Feature] #Updated ExperimentPage

--- frontend/src/pages/ExperimentPage/ExperimentPage.jsx
+++ frontend/src/pages/ExperimentPage/ExperimentPage.jsx
@@ -1,5 +1,4 @@
 import NewsOnly from "../../components/conditions/NewsOnly/NewsOnly";
-import MyTwoCents from "../../components/conditions/MyTwoCents/MyTwoCents";
 
 export default function ExperimentPage({
   assignment,
@@ -10,26 +9,12 @@ export default function ExperimentPage({
     return <p>実験内容を読み込んでいます...</p>;
   }
 
-  if (assignment.condition === "news") {
-    return (
-      <NewsOnly
-        topic={assignment.topic}
-        onPrevious={onPrevious}
-        onNext={onNext}
-      />
-    );
-  }
-
-  if (assignment.condition === "mytwocents") {
-    return (
-      <MyTwoCents
-        topic={assignment.topic}
-        pattern={assignment.pattern}
-        onPrevious={onPrevious}
-        onNext={onNext}
-      />
-    );
-  }
-
-  return <p>実験条件が正しく設定されていません。</p>;
+  return (
+    <NewsOnly
+      topic={assignment.topic}
+      article={assignment.article}
+      onPrevious={onPrevious}
+      onNext={onNext}
+    />
+  );
 }
