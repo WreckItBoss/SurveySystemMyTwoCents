2c5b773abe04a7218062014f04d602c89b127a52
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-09-03 18:36:33 +0900

    [Feature] #61 Updated experiment page so that it loads news only condition

--- frontend/src/pages/ExperimentPage/ExperimentPage.jsx
+++ frontend/src/pages/ExperimentPage/ExperimentPage.jsx
@@ -1,4 +1,5 @@
 import MyTwoCents from "../../components/conditions/MyTwoCents/MyTwoCents";
+import NewsOnly from "../../components/conditions/NewsOnly/NewsOnly";
 
 export default function ExperimentPage({
   assignment,
@@ -9,21 +10,31 @@ export default function ExperimentPage({
     return <p>実験内容を読み込んでいます...</p>;
   }
 
-  if (assignment.condition !== "mytwocents") {
+  if (assignment.condition === "mytwocents") {
     return (
-      <p>
-        実験条件が正しく設定されていません。
-      </p>
+      <MyTwoCents
+        topic={assignment.topic}
+        article={assignment.article}
+        pattern={assignment.pattern}
+        onPrevious={onPrevious}
+        onNext={onNext}
+      />
+    );
+  }
+
+  if (assignment.condition === "news") {
+    return (
+      <NewsOnly
+        article={assignment.article}
+        onPrevious={onPrevious}
+        onNext={onNext}
+      />
     );
   }
 
   return (
-    <MyTwoCents
-      topic={assignment.topic}
-      article={assignment.article}
-      pattern={assignment.pattern}
-      onPrevious={onPrevious}
-      onNext={onNext}
-    />
+    <p>
+      実験条件が正しく設定されていません。
+    </p>
   );
 }
